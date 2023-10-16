2574fc72466e1728454493694fc603a7309665c0
Zafer Balkan <zafer@zaferbalkan.com>
2023-10-16 22:40:04 +0300

    Implemented ValueObject in order to improve the GPInfo

--- PolViewer/GPInfo.cs
+++ PolViewer/GPInfo.cs
@@ -1,8 +1,17 @@
-﻿namespace PolViewer
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace PolViewer
 {
-    internal class GPInfo
+    internal class GPInfo : ValueObject<GPInfo>
     {
         internal string Name { get; set; }
         internal string Guid { get; set; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Name;
+            yield return Guid;
+        }
     }
 }
---
+++ PolViewer/ValueObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PolViewer
+{
+    public abstract class ValueObject<T> : System.IEquatable<ValueObject<T>>
+    where T : ValueObject<T>
+    {
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not T valueObject)
+                return false;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            return GetEqualityComponents().SequenceEqual(valueObject.GetEqualityComponents());
+        }
+
+        public override int GetHashCode() => GetEqualityComponents()
+                .Aggregate(1, (current, obj) =>
+                {
+                    unchecked
+                    {
+                        return (current * 23) + (obj?.GetHashCode() ?? 0);
+                    }
+                });
+
+        public bool Equals(ValueObject<T>? other) => Equals(other as T);
+
+        public static bool operator ==(ValueObject<T> a, ValueObject<T> b)
+        {
+            if (a is null && b is null)
+                return true;
+
+            if (a is null || b is null)
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(ValueObject<T> a, ValueObject<T> b)
+        {
+            return !(a == b);
+        }
+    }
+}
